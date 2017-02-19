af913a3f9e9855ac74bff1d38cbbb07b14f4d071
Grant Shively <gshively11@gmail.com>
2017-02-19 15:52:32 -0700

    fixed run script

--- my-scripts/run.sh
+++ my-scripts/run.sh
@@ -3,8 +3,8 @@
 # Run from root of project
 
 docker run -d \
-  --name="my-home-assistant" \
+  --device=/dev/ttyACM0:/dev/zwaveusbstick:rwm \
   -v /home/pi/home-assistant/my-config:/config \
   -v /etc/localtime:/etc/localtime:ro \
   --net=host my-home-assistant \
-  --device=/dev/ttyACM0:/zwaveusbstick:rwm
+  --name="my-home-assistant"
