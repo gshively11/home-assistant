430fab7e473f2453b1f8c7986546f2bb4bba56f5
Grant Shively <gshively11@gmail.com>
2017-02-19 18:44:41 -0700

    Disable dirty repo check during build

--- my-scripts/build.sh
+++ my-scripts/build.sh
@@ -2,9 +2,9 @@
 
 # Run from root of project
 
-if ! [ -z "$(git status --porcelain)" ]; then
-  echo "Git repo is dirty, cannot build."
-  exit 1
-fi
+#if ! [ -z "$(git status --porcelain)" ]; then
+#  echo "Git repo is dirty, cannot build."
+#  exit 1
+#fi
 
 docker build -f My-Dockerfile -t "my-home-assistant:$(git rev-parse --short HEAD)" .
